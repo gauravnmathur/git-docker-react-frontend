cca7f00f25c0283f354e5795413442cb569b03e4
Gaurav Mathur <gaurav@autonomic.ai>
2018-12-02 00:15:28 -0800

    Testing feature branch

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            The Eagle Has Landed. The Shit has Hit the Fan.
           </p>
           <a
             className="App-link"
